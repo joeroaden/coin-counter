8b751567b3f190e6717257c64e3e27cbd8def433
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-08-29 17:01:42 -0700

    refactor counter.cs with remaining biz logic

--- __tests__/counter.test.js
+++ __tests__/counter.test.js
@@ -1,10 +1,31 @@
-import  coinCounter  from '../src/counter.js';
+// import test from 'node:test';
+import {coinCounter} from '../src/counter.js';
 
 describe("coinCounter", () => {
   
   test('should verify user input is numbers only', () => {
-    const input = 4.99;
-    expect(typeof (input)).toEqual("number");
+    const counter = 4.99;
+    expect(typeof (counter)).toEqual("number");
+  });
+
+  test('should return counter if input is not a number', () => {
+    const counter = parseInt("");
+    expect(counter).toEqual(NaN);
+  });
+
+  test('should stop counter once counter equals zero', () => {
+    const counter = 0;
+    expect(counter).toEqual(0);
+  });
+
+  test('should remove all quarters from counter', () => {
+    const newCounter = .25;
+    expect(coinCounter(newCounter)).toEqual(expect.stringContaining("Quarters: 1, 0"));
+  });
+
+  test('should remove all dimes from counter', () => {
+    const newCounter = .10;
+    expect(coinCounter(newCounter)).toEqual(expect.stringContaining("Dimes: 1, 0"));
   });
 
 
--- src/counter.js
+++ src/counter.js
@@ -1,3 +1,28 @@
-export default coinCounter = (money) => {
-
+export  const coinCounter = (counter) => {
+  // counter = Math.ceil(counter.toFixed(2) * 100) / 100;
+  if (isNaN(counter)) {
+    return counter;
+  }
+  if (counter <= 0) {
+    return counter;
+  }
+  if (counter >= .25) {
+    const quarterCount = Math.floor(counter.toFixed(2) /.25);
+    return `Quarters: ${quarterCount}, ` + coinCounter(counter -(quarterCount * .25));
+  }
+  console.log(counter);
+  if (counter >= .10 && counter <.25) {
+    const dimeCount = Math.floor(counter.toFixed(2) /.10);
+    return `Dimes: ${dimeCount},` + coinCounter(counter - (dimeCount * .10));
+  }
+  if (counter >= .05 && counter <.05) {
+    const nickelCount = Math.floor(counter.toFixed(2) /.05);
+    return `Nickels: ${nickelCount},` + coinCounter(counter - (nickelCount * .05));
+  }
+  if (counter >= 0 && counter < .05){
+    return `Pennies: ${Math.ceil(counter.toFixed(2))}`;
+  }
+  // return `Pennies: ${Math.ceil(counter.toFixed(2))}`;
 };
+
+
